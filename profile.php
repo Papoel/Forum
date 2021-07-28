81893823721a25647665e13fa876fa243c6da6ef
Pascal Briffard <pascal.briffard@gmail.com>
2021-07-28 02:29:59 +0200

    update display of profile

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@ require 'controller/users/securityController.php';
 require 'controller/users/showUserProfileController.php';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <?php include 'includes/head.php'; ?>
 
 <body>
@@ -17,85 +17,53 @@ require 'controller/users/showUserProfileController.php';
             '</div>';
          die();
       } ?>
-   </div><br><br>
+   </div>
 
-   <?php
-   if (isset($getQuestionUser)) {
-   ?>
+   <main class="container-fluid bg-dark text-white">
+      <div class="d-flex justify-content-around flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+         <?php if (isset($getQuestionUser)) { ?>
+            <h1 class="h2">Tableau de bord - <?= $user_pseudo; ?></h1>
+            <p class="text-warning"><?= $user_firstname . ' ' . $user_lastname; ?></p>
+         <?php } ?>
+      </div>
+   </main>
+
+   <?php if (isset($getQuestionUser)) { ?>
       <div class="container">
          <div class="card">
             <div class="card-body">
-               <h4> @<?= $user_pseudo; ?> </h4>
+               <h4> <?= $user_pseudo; ?> </h4>
                <hr>
                <p><?= $user_firstname . ' ' . $user_lastname; ?></p>
             </div>
          </div>
          <br>
-         <?php
-            while ($question = $getQuestionUser->fetch()) {
-               ?>
-                  <!-- <div class="container"> -->
-                     <div class="card">
-                        <div class="card-header">
-                           <?= $question['title']; ?>
-                        </div>
-                        <div class="card-body">
-                           <?= $question['description']; ?>
-                        </div>
-                        <div class="card-footer">
-                           Par
-                           <?= $question['pseudo_author']; ?>
-                           le
-                           <?= $question['date_publication']; ?>
-                        </div>
-                     </div>
-                     <br><hr><br>
-                  <!-- </div> -->
-               <?php
-            }
-         ?>
-      </div>
-   <?php
-   }
-   ?>
-
-   <div class="container-fluid">
-      <div class="row">
-         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
-            <div class="position-sticky pt-3">
-               <ul class="nav flex-column">
-                  <li class="nav-item">
-                     <a class="nav-link dash-link" aria-current="page" href="#">
-                        Les questions posées
-                     </a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="#">
-                        <p class="dash-link"> Les réponses postées</p>
-                     </a>
-                  </li>
-               </ul>
-            </div>
-         </nav>
-         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-               <h1 class="h2">Tableau de bord - <?= $user_pseudo; ?></h1>
-               <!-- <div class="btn-toolbar mb-2 mb-md-0">
-                  <div class="btn-group me-2">
-                     <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                     <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+         <?php while ($question = $getQuestionUser->fetch()) { ?>
+            <!-- <div class="container"> -->
+            <div class="card">
+               <div class="card-header">
+                  <div class="me-auto d-flex justify-content-between">
+                     <?= $question['title']; ?>
+                     <a class="mx-3 btn btn-sm btn-outline-primary px-3" href="question.php?id=<?= $question['id']; ?>">Lire</a>
                   </div>
-                  <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                     <span data-feather="calendar"></span>
-                     This week
-                  </button>
-               </div> -->
+               </div>
+
+               <div class="card-body">
+                  <?= $question['description']; ?>
+               </div>
+               <div class="card-footer">
+                  Par
+                  <?= $question['pseudo_author']; ?>
+                  le
+                  <?= $question['date_publication']; ?>
+               </div>
             </div>
-            <canvas class="my-4 w-100" id="myChart" width="900" height="435"></canvas>
-            <h2>Section title</h2>
-         </main>
+            <br>
+            <hr><br>
+            <!-- </div> -->
+         <?php } ?>
       </div>
-   </div>
+   <?php } ?>
 
 </body>
 
